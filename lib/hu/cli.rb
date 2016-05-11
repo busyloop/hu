0623ed12edf444cfce7f57754df5f5e7a3d5676b
moe <moe@busyloop.net>
2016-05-11 11:36:51 +0200

    Read heroku api key from ENV['HEROKU_API_KEY'].

--- lib/hu/cli.rb
+++ lib/hu/cli.rb
@@ -5,15 +5,16 @@ require 'yaml'
 require 'platform-api'
 
 require 'hu/collab'
+require 'hu/deploy'
 
 module Hu
   class Cli < Optix::Cli
-    API_TOKEN = ENV['HEROKU_API_TOKEN']
+    API_TOKEN = ENV['HEROKU_API_KEY'] || ENV['HEROKU_API_TOKEN']
     Optix::command do
       text "Hu v#{Hu::VERSION} - Heroku Utility"
       if API_TOKEN.nil?
         text ""
-        text "\e[1mWARNING: Environment variable 'HEROKU_API_TOKEN' must be set.\e[0m"
+        text "\e[1mWARNING: Environment variable 'HEROKU_API_KEY' must be set.\e[0m"
       end
       opt :quiet, "Don't show progress bar", :default => false
       opt :version, "Print version and exit", :short => :none
@@ -22,7 +23,7 @@ module Hu
       end
       filter do
         if API_TOKEN.nil?
-          STDERR.puts "\e[0;31;1mERROR: Environment variable 'HEROKU_API_TOKEN' must be set.\e[0m"
+          STDERR.puts "\e[0;31;1mERROR: Environment variable 'HEROKU_API_KEY' must be set.\e[0m"
           exit 1
         end
       end
