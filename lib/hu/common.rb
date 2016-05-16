be006ec80b3cdd53ffe6cd5b2f1b17db2a491a1b
moe <moe@busyloop.net>
2016-05-17 01:31:01 +0200

    Add version check.

--- lib/hu/common.rb
+++ lib/hu/common.rb
@@ -1,3 +1,5 @@
+require 'blackbox/gem'
+
 module Hu
   API_TOKEN = ENV['HEROKU_API_KEY'] || ENV['HEROKU_API_TOKEN'] || Netrc.read['api.heroku.com']&.password
 end
@@ -9,3 +11,9 @@ class String
   end
 end
 
+version_info = BB::Gem.version_info(check_interval: 900)
+unless version_info[:installed_is_latest] == true
+  puts "\e[33;1mWARNING: \e[0mA newer version of #{version_info[:gem_name]} is available."
+  puts "         Please type '\e[1mgem install #{version_info[:gem_name]}\e[0m' to upgrade (v#{version_info[:gem_installed_version]} -> v#{version_info[:gem_latest_version]})."
+  sleep 1
+end
