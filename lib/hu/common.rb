9baeea56f0a6e0538bd6a4bb1539f9f6f438180b
moe <moe@busyloop.net>
2016-05-24 13:13:46 +0200

    Add a newline.

--- lib/hu/common.rb
+++ lib/hu/common.rb
@@ -15,6 +15,7 @@ end
 unless ENV['SKIP_VERSION_CHECK']
   version_info = BB::Gem.version_info(check_interval: 900)
   unless version_info[:installed_is_latest] == true
+    puts
     puts "\e[33;1mWoops! \e[0mA newer version of #{version_info[:gem_name]} is available."
     puts "       Please type '\e[1mgem install #{version_info[:gem_name]}\e[0m' to upgrade (v#{version_info[:gem_installed_version]} -> v#{version_info[:gem_latest_version]})."
     sleep 1
