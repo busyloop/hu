2ae2582f3fb7c732fdd8621e9b2777e9d3e6011c
moe <moe@busyloop.net>
2020-03-31 15:25:06 +0200

    Add rhythm

--- lib/hu/deploy.rb
+++ lib/hu/deploy.rb
@@ -584,8 +584,6 @@ EOF
 #
 #                 run_each(script, parser: parser, sigint_handler: sigint_handler)
 
-                print"\a"; sleep 0.4
-
                 # disabled until we find a way to stop terminal-notifier
                 # from spamming messages to stdout.
                 #
@@ -593,10 +591,10 @@ EOF
                 #                 title: 'Deploy successful',
                 #                 message: "Release #{release_tag} has been deployed to #{prod_app_name}")
 
-                puts "\e[42m  \e[0m \e[0;32mRelease \e[1m#{release_tag}\e[0;32m has been deployed to \e[1m#{prod_app_name}\e[0;32m\e[0m \e[42m  \e[0m"
-                print"\a"; sleep 0.4
-                puts
-                print"\a"; sleep 0.4
+                [0.3,0.15,0.12,0.3,0.55].each { |i| print "\a"; sleep i }
+                puts "\e[42m  \e[0m \e[0;32mRelease \e[1m#{release_tag}\e[0;32m has been deployed to \e[1m#{prod_app_name}\e[0;32m\e[0m \e[42m  \e[0m\a"
+                sleep 0.35
+                puts "\a"
 
                 exit 0
               end
